8b5c3e33d51056afa2957537e3edf0345e9de5f6
Benjamin Brewer MD <bjb109github@gmail.com>
2024-03-14 18:05:36 -0400

    Initial Commit

---
+++ picture2x/picture2xApp.swift
@@ -0,0 +1,17 @@
+//
+//  picture2xApp.swift
+//  picture2x
+//
+//  Created by Benjamin Brewer on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct picture2xApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
